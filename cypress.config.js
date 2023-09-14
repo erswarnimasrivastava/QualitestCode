3565cd15fa20f56badcc9be8a8842ad4dc64e6d3
erswarnimasrivastava <144949768+erswarnimasrivastava@users.noreply.github.com>
2023-09-14 09:45:22 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -14,30 +14,11 @@ module.exports = defineConfig({
     supportFile: 'cypress/support/e2e.js'
   },
   env: {
-    ARN_ATS_IMF: 'arn:aws:iam::886037714351:role/TestImfReceiptTestingInfrastructu-EndToEndTestRole-18BGC3HV3DQGC',
-    ARN_FBD_DEV: 'arn:aws:iam::537989602554:role/TestFbdTestingInfrastructure-EndToEndTestRole-J2FJCHRQCOB2',
-    ARN_FBD_MEDIA: 'arn:aws:iam::180790766189:role/TestFbdTestingInfrastructure-EndToEndTestRole-I2GS79XBJMKX',
-    CORE_NUMBER: 'TEST123A',
-    IMF_ARRIVALS_BUCKET: 'testimfreceiptsharedresources-arrivalsbucket-oobzyxydiv4s',
-    REGION: 'eu-west-1',
-    TEST_MEDIA_BUCKET: 'testfbdtestinginfrastructure-testmediabucket-190bcnionyagx',
+    
   },
   clientCertificates: [
     {
-      url: 'https://imf-receipt-commissions.test.api.bbci.co.uk',
-      certs: [
-        {
-          key: path.relative(process.env.IMF_REPO_PATH, process.env.BBC_CLIENT_KEY),
-          cert: path.relative(process.env.IMF_REPO_PATH, process.env.BBC_CLIENT_CERT)
-        },
-      ],
-      url: 'https://fbd-dashboard.test.tools.bbc.co.uk/',
-      certs: [
-        {
-          key: path.relative(process.env.IMF_REPO_PATH, process.env.BBC_CLIENT_KEY),
-          cert: path.relative(process.env.IMF_REPO_PATH, process.env.BBC_CLIENT_CERT)
-        },
-      ],
+    
     },
   ],
   chromeWebSecurity: false,
